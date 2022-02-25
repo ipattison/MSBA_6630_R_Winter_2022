3ff92f96748e957b2635d4a77342a2f0d11c8385
Israel J Pattison <israelpattison@Pembrook.local>
2022-02-25 02:19:12 +0000

    updates to script to download covid data, some downloaded data files and documentation that should change rarely if ever.

--- svi_covid_in_nc_project/R/download_covid_data.R
+++ svi_covid_in_nc_project/R/download_covid_data.R
@@ -7,6 +7,15 @@ if (!exists('PROJECT_CONFIG_LOADED')) {
   }
 }
 
+# Load necessary libraries
+Packages <- c("lubridate","tidyverse")
+pacman::p_load(char = Packages)
+
+cols_factor <- c("case_month", "res_county", "county_fips_code", "age_group", 
+          "sex", "race", "ethnicity", "process", "exposure_yn", 
+          "current_status", "symptom_status", "hosp_yn", "icu_yn", 
+          "death_yn", "underlying_conditions_yn")
+cols_num <- c("case_positive_specimen", "case_onset_interval")
 
 if (!dir.exists(DATA_PATH)) dir.create(DATA_PATH)
 if (!dir.exists(COVID_DATA_PATH)) dir.create(COVID_DATA_PATH)
@@ -15,7 +24,14 @@ if (!dir.exists(COVID_DATA_PATH)) dir.create(COVID_DATA_PATH)
 Packages <- c("RSocrata")
 pacman::p_load(char = Packages)
 
-covid_data <- read.socrata(COVID_URL)
+covid_data <- subset(read.socrata(COVID_URL))
+covid_data <- covid_data[-c(2, 3)]
+covid_data[cols_factor] <- lapply(covid_data[cols_factor], factor)
+for (col in cols_num)
+  covid_data[,col] <- as.numeric(covid_data[,col])
+covid_data$case_month <- ym(covid_data$case_month)
+covid_data$county <- toupper(covid_data$county)
+covid_data %>% group_by(res_county) %>%  summarize(CASE_COUNT = n())
 save(covid_data, file = COVID_RDATA_DST)
 
 rm(covid_data)
